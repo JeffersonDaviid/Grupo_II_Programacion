54221d3c018544ab3f9ed2ee158190f562402af1
Demonkinng <angel.chuncho@epn.edu.ec>
2022-11-21 21:01:25 -0500

    Cambios

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hola mundo, soy Jefferson David :D!");
         System.out.println("probrando...");
+        System.out.println("Hi!, soy Ángel");
     }
 }
