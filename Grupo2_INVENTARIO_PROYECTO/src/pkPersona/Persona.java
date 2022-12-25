fa3dcb8503cdaa374010fb2b615d27d4cb0f63bd
Jefferson David <jeffersonchile2001@gmail.com>
2022-12-25 13:25:43 -0500

    creacion clase persona

---
+++ Grupo2_INVENTARIO_PROYECTO/src/pkPersona/Persona.java
@@ -0,0 +1,9 @@
+public class Persona {
+    private final String nombre;
+    private final int codigo;
+
+    public Persona(String nombre, int codigo) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
+}
