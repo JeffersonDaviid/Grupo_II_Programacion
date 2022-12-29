1b0a9f65782dbfd62020365c2e8ebdf594948e8d
Jefferson David <jeffersonchile2001@gmail.com>
2022-12-28 21:05:14 -0500

    Revert "archivos iniciales"
    This reverts commit 4657f15c85223a0ffa2f061d77778670a6b70ff6.

--- Grupo2_INVENTARIO_PROYECTO/src/pkPersona/Persona.java
+++ Grupo2_INVENTARIO_PROYECTO/src/pkPersona/Persona.java
@@ -1,37 +1,9 @@
-package pkPersona;
-
 public class Persona {
     private final String nombre;
-    private final String cedula;
-    private int edad;
-    private final String sexo;
+    private final int codigo;
 
-    public Persona(String nombre, String cedula, int edad, String sexo) {
+    public Persona(String nombre, int codigo) {
         this.nombre = nombre;
-        this.cedula = cedula;
-        this.edad = edad;
-        this.sexo = sexo;
-    }
-
-    // GETTERS
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getCedula() {
-        return cedula;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    // SETTERS
-    public void setEdad(int edad) {
-        this.edad = edad;
+        this.codigo = codigo;
     }
 }
