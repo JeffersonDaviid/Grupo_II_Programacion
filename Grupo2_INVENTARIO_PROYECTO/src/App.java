fd8181b885636992612623a153212acde9111ca7
Jefferson <jeffersonchile2001@gmail.com>
2023-01-11 06:14:23 -0500

    base de datos en app

--- Grupo2_INVENTARIO_PROYECTO/src/App.java
+++ Grupo2_INVENTARIO_PROYECTO/src/App.java
@@ -1,11 +1,23 @@
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import UI_Interfaz.Disconected;
 import UI_Interfaz.IniciarSesion;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-        IniciarSesion app = new IniciarSesion();
-        app.setVisible(true);
-        // frame.setIconImage(new
-        // ImageIcon(getClass().getResource("images/logoEmpresa.png")).getImage());
+        try {
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/bd_usuarios", "root", "");
+            IniciarSesion app = new IniciarSesion();
+            app.setVisible(true);
+            conexion.close();
+        } catch (SQLException errorConectionInitial) {
+            Disconected d = new Disconected();
+            d.setVisible(true);
+            // // sudo /opt/lampp/lampp start -> inicar
+            // // sudo /opt/lampp/lampp stop -> parar
+        }
     }
 }
---
+++ Grupo2_INVENTARIO_PROYECTO/src/UI_Interfaz/Disconected.java
@@ -0,0 +1,42 @@
+package UI_Interfaz;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Image;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+
+public class Disconected extends JFrame {
+
+    public Disconected() {
+        setTitle("DESCONECTADO");
+        setBounds(0, 0, 800, 400);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        JLabel lbLeyendaDesconentado = new JLabel("Ups! Algo salio mal");
+        lbLeyendaDesconentado.setFont(new Font("Fira Code Medium", Font.BOLD | Font.ITALIC, 30));
+        lbLeyendaDesconentado.setHorizontalAlignment(SwingConstants.CENTER);
+        lbLeyendaDesconentado.setBounds(12, 30, 766, 38);
+        add(lbLeyendaDesconentado);
+
+        JLabel lbBackground = new JLabel();
+        lbBackground.setBounds(0, 0, 800, 372);
+        ImageIcon imageBackground = new ImageIcon("images/disconected_DataBase.png");
+        Icon iconBackgound = new ImageIcon(
+                imageBackground.getImage().getScaledInstance(lbBackground.getWidth(), lbBackground.getHeight(),
+                        Image.SCALE_SMOOTH));
+        lbBackground.setIcon(iconBackgound);
+        getContentPane().add(lbBackground);
+    }
+
+    // public static void main(String[] args) {
+    // Disconected d = new Disconected();
+    // d.setVisible(true); // EL SETVISIBLE VA OBLIGATORIAMENTE CUANDO SE LLAMA A
+    // // LA CLASE, esta aqui solo para probar la app de forma independiente
+    // }
+}
