bce9010fbb761702c7d048da4df4611264b62027
Arch Jefferson <Jeffersonchile2001@gmail.com>
2023-02-07 13:25:27 -0500

    api spaguetti

---
+++ src/DataAccess/DataHelperAPI.java
@@ -0,0 +1,63 @@
+package DataAccess;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class DataHelperAPI {
+    public static void main(String[] args) throws IOException {
+        try {
+
+            int numeroUsuario = 1;
+
+            URL url = new URL("https://jsonplaceholder.typicode.com/users/");
+            HttpURLConnection conn = (HttpsURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");
+
+            InputStream strm = conn.getInputStream();
+            byte[] arrayStream = strm.readAllBytes();
+
+            String contentJson = "";
+
+            for (byte tmp : arrayStream) {
+                contentJson += (char) tmp;
+            }
+            // System.out.println(contentJson);
+
+            JSONArray json = new JSONArray(contentJson);
+
+            for (Object obj : json) {
+                // SIMULANDO UN WHERE
+                if (Integer.parseInt((((JSONObject) obj).get("id")).toString()) == numeroUsuario) {
+
+                    // ACCEDER A UN OBJETO DE PRIMER NIVEL
+                    System.out.println("Usuario: " + ((JSONObject) obj).get("name").toString().toUpperCase());
+                    System.out.println("\t <> Email: "
+                            + ((JSONObject) obj).get("email").toString().toUpperCase());
+                    System.out.println("\t <> Phone: "
+                            + ((JSONObject) obj).get("phone").toString().toUpperCase());
+                    System.out.println("\t <> Website: "
+                            + ((JSONObject) obj).get("website").toString().toUpperCase());
+                    // ACCEDER A UN OBJETO DE SEGUNDO NIVEL
+                    System.out.println("\t <> Empresa: "
+                            + new JSONObject(((JSONObject) obj).get("company").toString()).get("name").toString()
+                                    .toUpperCase());
+                    System.out.println();
+                }
+            }
+
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/DataAccess/SQLiteDataHelper.java
@@ -0,0 +1,78 @@
+/*  Copyright (C) 2K23, patmic
+ *  pat_mic@hotmail.com
+ *  Version 1.0
+ */
+package DataAccess;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * <b> SQLiteDataHelper, </b> permite la gestion y el acceso a los datos a una base de datos SQLite3
+ */
+public abstract class SQLiteDataHelper {
+    private static String DBPathConnection = null;
+    private static Connection conn = null;
+    public SQLiteDataHelper(String  dbPathConnection){
+        // Definir la cadena de conneccion : jdbc:sqlite:data\\TinderPet.db
+        if (!dbPathConnection.isEmpty())
+            SQLiteDataHelper.DBPathConnection = dbPathConnection;
+    }
+    /**
+     * getDBConnection, obtiene el cadena de coneccion a la base de datos
+     * @return
+     */
+    protected String getDBConnection() {
+        return DBPathConnection;
+    }
+
+    /**
+     * <b>connSQLITE</b>, permite crear una sola instancia-Singeton para la coneccion a la base de datos
+     * @return retorna coneccion a la base de datos definida en el contructor
+     * @throws SQLException
+     */
+    protected static Connection getConnection() throws SQLException{
+        if(conn == null)
+            conn = DriverManager.getConnection(DBPathConnection);
+        return conn;
+    }
+
+    protected static ResultSet getResultSet(String sql) throws SQLException     
+    {
+        Connection conn = null;
+        Statement  stmt = null;
+        ResultSet  rs   = null;
+       // try {
+            conn = getConnection();            // jdbc:sqlite:data\\TinderPet.db 
+            stmt = conn.createStatement();     // CRUD : select * ...
+            rs   = stmt.executeQuery(sql);     //
+        // } 
+        // catch (SQLException e) {
+        //     System.out.println(e.getMessage());
+        //     throw new AppException(APP.GLOBAL.DB_EXCEPTION,"Failed to connect to database", e);
+        // }
+        return rs;
+    }
+
+
+     
+
+// public void onOpen(SQLiteDatabase db) {}
+//     /**
+//      * Close any open database object.
+//      */
+//     public synchronized void close() {
+//         if (mIsInitializing) throw new IllegalStateException("Closed during initialization");
+//         if (mDatabase != null && mDatabase.isOpen()) {
+//             mDatabase.close();
+//             mDatabase = null;
+//         }
+//     }
+}
+
+/*
+ * ref: https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/database/sqlite/SQLiteOpenHelper.java
+ */
